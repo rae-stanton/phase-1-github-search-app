aab3abc7e8bbd0f8615ef8c460cecc85a18bec87
Rae Stanton <raestanton93@gmail.com>
2023-08-11 08:41:01 -0700

    Adds both user & repo list
    This takes a single user and displays up to ten (if they have them) repos they have publicly available.

--- js/index.js
+++ js/index.js
@@ -1,40 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("github-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
 
-      const searchTerm = document.getElementById("search").value;
+  document.getElementById("github-search-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const searchTerm = document.getElementById("github-search").value;
+    const userEndpoint = `https://api.github.com/search/users?q=${searchTerm}`;
 
-      const endpoint = `https://api.github.com/search/users?q=${searchTerm}`;
+    fetch(userEndpoint, {
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      const userList = document.getElementById("user-list");
+      userList.innerHTML = "";
 
-      fetch(endpoint, {
-        method: "GET",
-        headers: {
-          Accept: "application/vnd.github.v3+json",
-        },
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          const userList = document.getElementById("user-list");
-          userList.innerHTML = ""; // Clear any previous results
+      if (data.items.length > 0) {
+        const username = data.items[0].login;
 
-          data.items.forEach((user) => {
-            const li = document.createElement("li");
-            li.textContent = user.login;
-            userList.appendChild(li);
-          });
-        })
-        .catch((error) => {
-          console.log(
-            "There was a problem with the fetch operation:",
-            error.message
-          );
+        // Display the username
+        const userLi = document.createElement("li");
+        userLi.textContent = username;
+        userList.appendChild(userLi);
+
+        return fetch(`https://api.github.com/users/${username}/repos`, {
+          method: "GET",
+          headers: {
+            Accept: "application/vnd.github.v3+json",
+          },
         });
+      } else {
+        throw new Error("No user found");
+      }
+    })
+    .then(response => response.json())
+    .then(repos => {
+      const repoList = document.getElementById("repos-list");
+      repoList.innerHTML = "";
+
+      repos.slice(0, 10).forEach((repo) => {
+        const li = document.createElement("li");
+        li.textContent = repo.name;
+        repoList.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.log("Error:", error.message);
     });
+  });
+
 });
